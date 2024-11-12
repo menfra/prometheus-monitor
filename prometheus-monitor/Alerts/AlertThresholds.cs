aa99a5eb671d6794fd0980a76c845c691d3d861b
Frank Mensah <33913399+menfra@users.noreply.github.com>
2024-11-12 22:13:27 +0100

    [PROM499] Log concurrency clearance

---
+++ prometheus-monitor/Alerts/AlertThresholds.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prometheus_monitor.Alerts
+{
+    public class AlertThresholds
+    {
+        public int LoginFrequencyThreshold { get; set; }
+        public int IPChangeThreshold { get; set; }
+        public double AccessPatternDeviation { get; set; }
+    }
+}
